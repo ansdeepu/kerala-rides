fd4ed098bfd4bab61464a9aa148e9e90e79c59a0
Deepu S, Personal <ansdeepu@gmail.com>
2025-11-10 08:31:41 +0000

    Error

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,18 +3,18 @@
 
 // Get the configuration from environment variables
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "YOUR_API_KEY_HERE",
+  authDomain: "YOUR_AUTH_DOMAIN_HERE",
+  projectId: "YOUR_PROJECT_ID_HERE",
+  storageBucket: "YOUR_STORAGE_BUCKET_HERE",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID_HERE",
+  appId: "YOUR_APP_ID_HERE",
 };
 
 // A function to check if the config is valid.
 // It checks if all the required environment variables are set.
 function isConfigValid(config: any): boolean {
-  return Object.values(config).every((value) => value);
+  return !Object.values(config).some(value => value.startsWith('YOUR_'));
 }
 
 // If the config is not valid, we log an error to the console.
